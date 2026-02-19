39526deebbe8878952ace52b12be7bbbb9dd875c
lganeshmoorthi2005-cmyk <lganeshmoorthi2005@gmail.com>
2026-02-19 10:28:53 +0530

    Add files via upload

---
+++ operator task.py
@@ -0,0 +1,73 @@
+print("hello world")
+g=10
+n=30
+print(g+n)
+print(g-n)
+print(g*n)
+print(g/n)
+print(g//n)
+print(g%n)
+print(5**0.5)
+print("square root",n**0.5)
+
+a=30
+b=20
+print("Area of triangle",0.5*a*b)
+
+p=20
+q=30
+print("quadratic Equation",(p+q)**2)
+print("quadratic Equation",(p-q)**2)
+print("quadratic Equation",(p**2-q**2))
+
+p=10
+q=20
+print("before swapping",p,q)
+r=p
+p=q
+q=r
+print("after swapping of p & q",p,q)
+
+k=10
+print("kilometer to miles",(k*0.63))
+      
+c=30
+print("celsius to fahrenheit",(c*1.8)+32)
+
+a=326
+print("last digit",(a%10))
+
+b=656
+print("last two digit",(b%20))
+
+a=24567
+a//=100
+n=a%10
+print("square root",a,(n*n))
+
+b=34567
+b//=200
+n=b%10
+print("digit",b,(n**2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
